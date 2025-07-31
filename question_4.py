df9bfa79b1225e65b64f62a08f0403568e83debf
IsaacDev14 <mwitiisaac14@gmail.com>
2025-07-31 13:40:47 +0300

    complete quiz 4 and 5

--- question_4.py
+++ question_4.py
@@ -21,17 +21,25 @@ It should return the cost of changing the sign from the old message to the new m
 
 
 def estimate(add_cost, remove_cost, old_sign, new_sign):
-    return 0
+    m = len(old_sign)
+    n = len(new_sign)
 
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
 
+    for i in range(m + 1):
+        dp[i][0] = i * remove_cost  
+    for j in range(n + 1):
+        dp[0][j] = j * add_cost   
 
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if old_sign[i - 1] == new_sign[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                cost_remove = dp[i - 1][j] + remove_cost
+                cost_add = dp[i][j - 1] + add_cost
+                cost_substitute = dp[i - 1][j - 1] + remove_cost + add_cost
+                dp[i][j] = min(cost_remove, cost_add, cost_substitute) # We take the minimum of the three possible operations.
 
-
-
-
-
-
-
-
-
+    return dp[m][n]
 
